e66314397dd72c62976bd739c0628aeec0b4654f
Artem Pavlenko <artem@mapnik.org>
2017-09-19 11:43:49 +0200

    add simple script to generate animations

---
+++ generate_animation.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+CMD="./bin/clang-linux-6.0.0/release/comparable-distance-test"
+count=0
+for i in `seq -20 30`; do
+    let x=0
+    let y=i*2
+    echo $CMD data/poly-multihole.json $x $y
+    $CMD data/poly-multihole.json $x $y
+    svg2png point_to_geometry_distance.svg
+    printf -v frame "%03d" $count
+    convert -background white -flatten point_to_geometry_distance.png output/point_to_geometry_distance-$frame.png
+    let count+=1
+done
+
+convert -delay 25 -loop 0 output/point_to_geometry_distance*.png point_to_geometry_distance.gif
+rm output/*
